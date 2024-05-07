be8ea6b593a121c0986b77f1e7795ab1b42efa0e
Giovana Oliveira <gioliveira14@outlook.com>
2024-05-07 00:19:06 -0300

    feat: desenvolvimento da home

---
+++ js/slick.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  $(".funcionalidades-carrossel").slick({
+    infinite: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    speed: 300,
+    arrows: true,
+  });
+});
