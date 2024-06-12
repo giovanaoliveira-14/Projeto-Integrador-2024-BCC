768fd28d390e9b07fffc5ae8365918b126e23d9c
Giovana Oliveira <gioliveira14@outlook.com>
2024-06-11 22:40:33 -0300

    chore: desenvolvimento de login e cadastro de usuário

--- public/js/home.js
+++ public/js/home.js
@@ -1,72 +1,87 @@
-// Substitua pelo seu próprio API Key da OpenWeather
+if (localStorage.getItem("userLoggedIn") === "true") {
+  // Substitua pelo seu próprio API Key da OpenWeather
   function getWeatherData(city) {
-    const apiKey = "ceee946a10e4c4c391957c0d3f01150d"; 
+    const apiKey = "ceee946a10e4c4c391957c0d3f01150d";
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&lang=pt_br&appid=${apiKey}`;
 
     fetch(url)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Erro ao obter dados do clima');
-            }
-            return response.json();
-        })
-        .then(data => {
-            // Manipular os dados recebidos e atualizar o HTML
-            console.log(data);
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Erro ao obter dados do clima");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        // Manipular os dados recebidos e atualizar o HTML
+        console.log(data);
 
-            // Atualizar a temperatura atual
-            document.querySelector('.informacoes-temperatura h3').textContent = `${data.main.temp}°`;
+        // Atualizar a temperatura atual
+        document.querySelector(
+          ".informacoes-temperatura h3"
+        ).textContent = `${data.main.temp}°`;
 
-            // Atualizar a temperatura mínima
-            document.getElementById('temperaturaMin').textContent = `${data.main.temp_min}° Celsius`;
+        // Atualizar a temperatura mínima
+        document.getElementById(
+          "temperaturaMin"
+        ).textContent = `${data.main.temp_min}° Celsius`;
 
-            // Atualizar a umidade
-            document.getElementById('umidade').textContent = `${data.main.humidity}%`;
+        // Atualizar a umidade
+        document.getElementById(
+          "umidade"
+        ).textContent = `${data.main.humidity}%`;
 
-            // Atualizar outras informações conforme necessário
-            // ...
+        // Atualizar outras informações conforme necessário
+        // ...
 
-            // Calcular e atualizar o índice UV (exemplo)
-            const intensity = data.current.uvi;
-            const uvIndex = calculateUVIndex(intensity);
-            document.getElementById('luzUV').textContent = uvIndex;
+        // Calcular e atualizar o índice UV (exemplo)
+        const intensity = data.current.uvi;
+        const uvIndex = calculateUVIndex(intensity);
+        document.getElementById("luzUV").textContent = uvIndex;
 
-    
-            const weatherDescription = translateWeatherDescription(data.weather[0].description);
-            document.getElementById('descricaoClima').textContent = weatherDescription;
-        })
-        .catch(error => {
-            console.error('Erro:', error);
-        });
-}
+        const weatherDescription = translateWeatherDescription(
+          data.weather[0].description
+        );
+        document.getElementById("descricaoClima").textContent =
+          weatherDescription;
+      })
+      .catch((error) => {
+        console.error("Erro:", error);
+      });
+  }
 
-// Função para calcular o índice UV (exemplo)
-function calculateUVIndex(intensity) {
+  // Função para calcular o índice UV (exemplo)
+  function calculateUVIndex(intensity) {
     if (intensity < 3) {
-        return 'Baixo';
+      return "Baixo";
     } else if (intensity < 6) {
-        return 'Moderado';
+      return "Moderado";
     } else if (intensity < 8) {
-        return 'Alto';
+      return "Alto";
     } else if (intensity < 11) {
-        return 'Muito Alto';
+      return "Muito Alto";
     } else {
-        return 'Extremo';
+      return "Extremo";
     }
-}
+  }
 
-// Função para traduzir as descrições do clima (exemplo)
-function translateWeatherDescription(description) {
+  // Função para traduzir as descrições do clima (exemplo)
+  function translateWeatherDescription(description) {
     switch (description) {
-        case 'clear sky':
-            return 'Céu Limpo';
-        case 'few clouds':
-            return 'Poucas Nuvens';
-        // Adicione mais traduções conforme necessário
-        default:
-            return description;
+      case "clear sky":
+        return "Céu Limpo";
+      case "few clouds":
+        return "Poucas Nuvens";
+      // Adicione mais traduções conforme necessário
+      default:
+        return description;
     }
-}
+  }
 
-// Exemplo de uso: Chamando a função com o nome da cidade desejada
-getWeatherData('Marília');
+  // Exemplo de uso: Chamando a função com o nome da cidade desejada
+  getWeatherData("Marília");
+} else {
+  alert("Usuário não logado!");
+  setTimeout(function () {
+    window.location.href = "../index.html";
+  }, 500);
+}
--- public/js/login.js
+++ public/js/login.js
@@ -1,4 +1,4 @@
-
+// Configuração Firebase
 const firebaseConfig = {
     apiKey: "AIzaSyAQNunOvOyP6Z3hShU3BV8KazyNwEjd_0c",
     authDomain: "projeto-estacao-meteorologica.firebaseapp.com",
@@ -8,57 +8,119 @@ const firebaseConfig = {
     messagingSenderId: "1031346489742",
     appId: "1:1031346489742:web:53be9814f15dc27a837cdf",
     measurementId: "G-X68BBJH2WT"
-};
-
-firebase.initializeApp(firebaseConfig);
-const database = firebase.database();
-console.log(database)
-
-// const emailUsuario = document.querySelector("#email-user");
-// const senhaUsuario = document.querySelector("#password-user");
-// const buttonLogin = document.querySelector("#btn-formLogin");
-
-// function decryptPassword(encryptedPassword, secretKey) {
-//   const bytes = CryptoJS.AES.decrypt(encryptedPassword, secretKey);
-//   const originalPassword = bytes.toString(CryptoJS.enc.Utf8);
-//   return originalPassword;
-// }
-
-// buttonLogin.addEventListener("click", function (e) {
-//   e.preventDefault();
-//   loginUser(emailUsuario.value, senhaUsuario.value);
-// });
-
-// function loginUser(email, password) {
-//   const usersRef = firebase.database().ref("Usuario");
-//   usersRef
-//     .once("value")
-//     .then((snapshot) => {
-//       const users = snapshot.val();
-//       for (const userId in users) { 
-//         const user = users[userId];
-//         const senhaDescriptografada = decryptPassword(
-//           user.senha,
-//           "pR0Jet01nt&gr@d0R02!"
-//         );
-//         if (user.email === email) {
-//           if (senhaDescriptografada === password || user.senha === password) {
-//             localStorage.setItem("userLoggedIn", "true");
-//             const userData = {
-//               userId: userId,
-//             };
-//             const userDataJson = JSON.stringify(userData);
-//             localStorage.setItem("userData", userDataJson);
-//             setTimeout(function () {
-//               window.location.href = "../home.html";
-//             }, 1000);
-//           } else {
-//             alert("Credenciais inválidas.");
-//           }
-//         }
-//       }
-//     })
-//     .catch((error) => {
-//       alert("Erro ao acessar o banco de dados:", error);
-//     });
-// }
+  };
+  firebase.initializeApp(firebaseConfig);
+  const database = firebase.database();
+  
+  // Funções
+  function validateEmail(email) {
+    return /\S+@\S+\.\S+/.test(email);
+  }
+  
+  function decryptPassword(encryptedPassword, secretKey) {
+    const bytes = CryptoJS.AES.decrypt(encryptedPassword, secretKey);
+    return bytes.toString(CryptoJS.enc.Utf8);
+  }
+  
+  function encryptPassword(password, secretKey) {
+    return CryptoJS.AES.encrypt(password, secretKey).toString();
+  }
+  
+  function showAlert(message, redirectUrl = null) {
+    alert(message);
+    if (redirectUrl) {
+      setTimeout(() => {
+        window.location.href = redirectUrl;
+      }, 1000);
+    }
+  }
+  
+  // Cadastro
+  const cadNome = document.querySelector("#cadastro-nome");
+  const cadEmail = document.querySelector("#cadastro-email");
+  const cadSenha = document.querySelector("#cadastro-senha");
+  const cadEndereco = document.querySelector("#cadastro-endereco");
+  const cadBairro = document.querySelector("#cadastro-bairro");
+  const cadCidade = document.querySelector("#cadastro-cidade");
+  const cadEstado = document.querySelector("#cadastro-estados");
+  const cadButton = document.querySelector("#cadastro-button");
+  
+  cadButton.addEventListener("click", function (e) {
+    e.preventDefault();
+    if (!validateEmail(cadEmail.value)) {
+      showAlert("Email inválido.");
+      return;
+    }
+    const encryptedPassword = encryptPassword(cadSenha.value, "pR0Jet01nt&gr@d0R02!");
+    const newUsuario = createUsuario(
+      cadNome.value,
+      cadEmail.value,
+      encryptedPassword,
+      cadEndereco.value,
+      cadBairro.value,
+      cadCidade.value,
+      cadEstado.value
+    );
+  
+    if (newUsuario) {
+      showAlert("Seu cadastro foi realizado com sucesso! \nFaça o seu login para entrar na plataforma!", "../login-cadastro.html");
+    } else {
+      showAlert("Não foi possível realizar o seu cadastro :(");
+    }
+  });
+  
+  function createUsuario(nome, email, senha, endereco, bairro, cidade, estado) {
+    const newUserID = firebase.database().ref().child("Usuario").push().key;
+    const userData = {
+      userId: newUserID,
+      name: nome,
+      email: email,
+      senha: senha,
+      endereco: endereco,
+      bairro: bairro,
+      cidade: cidade,
+      estado: estado
+    };
+    firebase.database().ref(`Usuario/${newUserID}`).set(userData);
+    return true;
+  }
+  
+  // Login
+  const logUsuario = document.querySelector("#email-usuario");
+  const logSenha = document.querySelector("#senha-usuario");
+  const logButton = document.querySelector("#button-login");
+  
+  logButton.addEventListener("click", function (e) {
+    e.preventDefault();
+    loginUser(logUsuario.value, logSenha.value);
+  });
+  
+  function loginUser(email, password) {
+    if (!validateEmail(email)) {
+      showAlert("Email inválido.");
+      return;
+    }
+    const usersRef = firebase.database().ref("Usuario");
+    usersRef.once("value").then((snapshot) => {
+      const users = snapshot.val();
+      let userFound = false;
+      for (const userId in users) {
+        const user = users[userId];
+        const senhaDescriptografada = decryptPassword(user.senha, "pR0Jet01nt&gr@d0R02!");
+        if (user.email === email && (senhaDescriptografada === password || user.senha === password)) {
+          userFound = true;
+          localStorage.setItem("userLoggedIn", "true");
+          const userData = { userId: userId };
+          localStorage.setItem("userData", JSON.stringify(userData));
+          showAlert("Login bem-sucedido!", "../home.html");
+          break;
+        }
+      }
+      if (!userFound) {
+        showAlert("Email ou senha incorretos.");
+      }
+    }).catch((error) => {
+      showAlert("Erro ao acessar o banco de dados: " + error.message);
+    });
+  }
+  
